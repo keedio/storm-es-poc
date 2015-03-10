2601b47c64ebb4ab6b8c2fe10d21ad38d1c1c3be
rolmovel <rolmovel@gmail.com>
2015-03-10 14:37:04 +0100

    -m “dynamic type and json fields”

--- src/main/java/org/buildoop/storm/bolts/KafkaParserBolt.java
+++ src/main/java/org/buildoop/storm/bolts/KafkaParserBolt.java
@@ -6,9 +6,12 @@ import backtype.storm.topology.IBasicBolt;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Tuple;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import java.util.Date;
+import java.util.Iterator;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -19,7 +22,9 @@ import org.elasticsearch.common.joda.time.format.ISODateTimeFormat;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 import org.json.simple.parser.JSONParser;
+
 import java.util.UUID;
+
 import static backtype.storm.utils.Utils.tuple;
 
 @SuppressWarnings("serial")
@@ -55,23 +60,21 @@ public class KafkaParserBolt implements IBasicBolt {
 				JSONObject jsonObject = (JSONObject) obj;
 				String message = (String) jsonObject.get("message");
 				JSONObject extraData = (JSONObject) jsonObject.get("extraData");
-
+				Iterator iter = extraData.entrySet().iterator();
+				
+				while(iter.hasNext()){
+				      Map.Entry entry = (Map.Entry)iter.next();
+				      objAux.put((String)entry.getKey(), entry.getValue());
+				}
+				
 				objAux.put("message",message);
 				
 				
-				objAux.put("FunctionalEnvironment",extraData.get("FunctionalEnvironment"));
-				objAux.put("CI ID",extraData.get("CI ID"));
-				objAux.put("CI Name",extraData.get("CI Name"));
-				objAux.put("Delivery",extraData.get("Delivery"));
-				objAux.put("Company",extraData.get("Company"));
-				objAux.put("Hostname",extraData.get("Hostname"));
-
 				log.debug(message);
 
 				int inicio = message.indexOf("keedio.datagenerator: ")+"keedio.datagenerator: ".length();
 
 				objAux.put("timestamp",this.transformDate(message.substring(inicio, inicio + 23), "yyyy-MM-dd HH:mm:ss.SSS", "yyyy-MM-dd'T'HH:mm:ss.SSSZ"));
-				objAux.put("vdc", extraData.get("vdc"));
 
 			} catch (org.json.simple.parser.ParseException e) {
 				//e.printStackTrace();
@@ -80,7 +83,7 @@ public class KafkaParserBolt implements IBasicBolt {
 				log.error("Error al formatear la fecha. Revisar formato de mensaje");
 			}
 
-			collector.emit(tuple(String.valueOf(UUID.randomUUID()),index, (String)objAux.get("Delivery"), objAux.toString()));
+			collector.emit(tuple(String.valueOf(UUID.randomUUID()),index, (String)type, objAux.toString()));
 		}
 
 
